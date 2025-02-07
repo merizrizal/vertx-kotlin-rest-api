37de0416cc39aa904e54f648ea003ea712085a99
merizrizal <meriz.rizal@gmail.com>
2025-02-07 19:54:19 +0700

    use GraalVM JDK instead of JDK; add build native-image to gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,23 @@
+import org.gradle.jvm.tasks.Jar
+
 buildscript {
-    System.setProperty("kotlinVersion", "2.0.20")
-    System.setProperty("vertxVersion", "4.5.10")
+    System.setProperty("kotlinVersion", "2.1.0")
+    System.setProperty("vertxVersion", "4.5.12")
 }
 
 group = "com.merizrizal"
 version = "0.0.1"
 
+val useJava = 23
 val vertxVersion: String by System.getProperties()
+val appReleaseName = "${rootProject.name}-${version}"
 
 plugins {
     val kotlinVersion: String by System.getProperties()
 
     kotlin("jvm").version(kotlinVersion)
     id("java")
-    id("java-library")
+    id("org.graalvm.buildtools.native").version("0.10.4")
 }
 
 repositories {
@@ -36,7 +40,10 @@ dependencies {
     implementation("io.vertx:vertx-rx-java3-gen:$vertxVersion")
 }
 
-tasks.jar {
+tasks.register<Jar>("kotlinJar") {
+    group = "build"
+    description = "Kotlin jar"
+
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
     manifest {
@@ -48,22 +55,49 @@ tasks.jar {
     val sourcesMain = sourceSets.main.get()
     val contents = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
     from(contents)
+
+    dependsOn(tasks.classes)
 }
 
-tasks.register("cleanAndJar") {
+tasks.register<JavaExec>("runJar") {
     group = "build"
-    description = "Clean and create jar"
+    description = "Run the jar file"
 
-    dependsOn(tasks.clean)
-    dependsOn(tasks.jar)
+    classpath("${layout.buildDirectory.get().asFile.path}/libs/$appReleaseName.jar")
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(useJava))
     }
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(useJava)
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(useJava))
+            })
+            imageName.set("${appReleaseName}.run")
+            mainClass.set("AppKt")
+            fallback.set(false)
+            verbose.set(true)
+            sharedLibrary.set(false)
+
+            val path = "${projectDir}/src/main/resources/META-INF/native-image"
+            buildArgs.add("-H:ReflectionConfigurationFiles=${path}/reflect-config.json")
+            buildArgs.add("-H:EnableURLProtocols=http,https")
+            buildArgs.add("-H:+InstallExitHandlers")
+            buildArgs.add("-H:+ReportUnsupportedElementsAtRuntime")
+            buildArgs.add("-H:+ReportExceptionStackTraces")
+            buildArgs.add("--enable-http")
+            buildArgs.add("--enable-https")
+            buildArgs.add("--allow-incomplete-classpath")
+            buildArgs.add("--report-unsupported-elements-at-runtime")
+        }
+    }
 }
